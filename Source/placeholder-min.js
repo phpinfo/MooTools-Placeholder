4142f134e054ac2682e8bbbfec80cc943d92f731
Alexey <italianwind@gmail.com>
2010-03-18 20:32:11 +0300

    Compressed version

--- Source/placeholder-min.js
+++ Source/placeholder-min.js
@@ -10,8 +10,12 @@ authors:
 requires:
   core/1.2.4: '*'
 
-provides: none
+provides:
+  NS.Placeholder
 
 ...
 */
-$(window).addEvent("domready",function(){"placeholder"in document.createElement("input")||$$("input").each(function(a){var b=a.get("placeholder"),d=a.getStyle("color");if(b){a.setStyle("color","#aaa").set("value",b).addEvent("focus",function(){if(a.value==""||a.value==b)a.setStyle("color",d).set("value","")}).addEvent("blur",function(){if(a.value==""||a.value==b)a.setStyle("color","#aaa").set("value",b)});var c=a.getParent("form");c&&c.addEvent("submit",function(){a.value==b&&a.set("value","")})}})});
+var NS=NS||{};
+NS.Placeholder=new Class({Implements:[Options],options:{elements:"input[type=text]",cssClass:"placeholder",color:null},initialize:function(b){if(!("placeholder"in document.createElement("input"))){this.setOptions(b);switch($type(this.options.elements)){case "string":b=$$(this.options.elements);break;case "element":b=[this.options.elements];break;default:b=this.options.elements}b.each(function(a){var c=a.get("placeholder");if(c){a.store("ns-placeholder-text",c);a.store("ns-placeholder-color",a.getStyle("color"));
+this.blur(a);a.addEvents({focus:function(){this.focus(a)}.bind(this),blur:function(){this.blur(a)}.bind(this)});var d=a.getParent("form");d&&d.addEvent("submit",function(){a.value==c&&a.set("value","")})}}.bind(this))}},focus:function(b,a){a=a==undefined||a;var c=b.retrieve("ns-placeholder-text"),d=b.get("value");if(d==""||d==c){if(this.options.cssClass)b[a?"removeClass":"addClass"](this.options.cssClass);if(this.options.color)b.setStyle("color",a?b.retrieve("ns-placeholder-color"):this.options.color);
+b.set("value",a?"":c)}},blur:function(b){this.focus(b,false)}});
